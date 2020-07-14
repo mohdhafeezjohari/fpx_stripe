a0491764eb057f48e79d7f52456743b720d3d088
Mohd Hafeez <mohd.hafeez.johari@gmail.com>
2020-07-15 00:18:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
 
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Stripe Demo</title>
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 </head>
 
 <body>
-    <div class="container">
-        <form method="POST" action="process_index.php" role="molpayseamless">
-            <h1 style="margin-top:40px;">Billing Information</h1>
-            <div class="row">
-                <div class="col-md-12 marginbttm"> 
-                    <span class="mandatory">Amount</span>  
-                    <input type="text" class="form-control" name="total_amount" id="total_amount" value="2.00" required>
-                </div>
-                <hr />
-                <button type="submit" class="btn btn-primary pull-right">Pay with FPX</button>
+    <form method="POST" action="process_index.php">
+        <h1>Billing Information</h1>
+        <div class="row">
+            <div> 
+                <span>Amount</span>  
+                <input type="text" name="total_amount" value="2.00" required>
             </div>
-        </form>
-    </div>
+            <hr />
+            <button type="submit">Pay with FPX</button>
+        </div>
+    </form>
 </body>
 </html>
