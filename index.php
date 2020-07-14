541661d762a8c780eee271eb69d6ae0debb43c59
Mohd Hafeez <mohd.hafeez.johari@gmail.com>
2020-07-15 00:21:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
-
-<html>
-<head>
-    <title>Stripe Demo</title>
-</head>
-
-<body>
-    <form method="POST" action="process_index.php">
-        <h1>Billing Information</h1>
-        <div class="row">
-            <div> 
-                <span>Amount</span>  
-                <input type="text" name="total_amount" value="2.00" required>
-            </div>
-            <hr />
-            <button type="submit">Pay with FPX</button>
+<form method="POST" action="process_index.php">
+    <h1>Billing Information</h1>
+    <div class="row">
+        <div> 
+            <span>Amount</span>  
+            <input type="text" name="total_amount" value="2.00" required>
         </div>
-    </form>
-</body>
-</html>
+        <hr />
+        <button type="submit">Pay with FPX</button>
+    </div>
+</form>
